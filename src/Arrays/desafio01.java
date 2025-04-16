cc933fc1f2d15f334895451ef382ff2d4ab00844
kauanAfonso <kauanafon3@gmail.com>
2025-04-16 16:45:37 -0300

    Desafio 01 -> Desafio de array sobre nomes que mais aparecem em um array sem HashMap

--- src/Arrays/desafio01.java
+++ src/Arrays/desafio01.java
@@ -9,36 +9,54 @@ public class desafio01 {
         ArrayList<String> nomes = new ArrayList<String>();
         ArrayList<Integer> quantidade = new ArrayList<Integer>();
         Scanner scanner = new Scanner(System.in);
+        ArrayList<String> nomes_unicos = new ArrayList<String>();
         String escolha;
+        Integer contador_nome;
 
+        //Escolha do usuario
         do {
             System.out.println("Digite um nome para a lista ou -1 para sair");
-            escolha = scanner.next();
+            escolha = scanner.next().toLowerCase();
 
-            if(escolha == "-1"){
+            if(escolha.equals("-1")){
                 break;
             }
-            nomes.add(escolha);
+            nomes.add(escolha); //adiiconando nome que o usuario digitou em nomes
+        }while (!escolha.equals("-1")); //Enquanto o usuairo não manda '-1'
 
-            //Com dois For's talvez sja possivel
-            for (int i = 0; i < nomes.size(); i++) {
 
-                for (int j = 0; j < nomes.size(); j++) {
-                    boolean verificar_nome = false;
-                    if(nomes.get(j) == escolha){
-                        nomes.add(escolha);
-                        verificar_nome = true;
-                    }
+        //for para percorrer a lista de nomes
+        for (int i = 0; i < nomes.size(); i++) {
 
+            String nome_atual = nomes.get(i);//pegando o nome_atual
+            contador_nome = 0; //contador_nome recebe 0
+            boolean nome_contado = false; //nome contado -> para verificar se o nome ja foi contado
+
+            //For Each -> para verificar se o nome_atual
+            //já foi contado (ou seja ele está presente em arr(nomes_unicos))
+            for (String nome: nomes_unicos){
+                if(nome.equals(nome_atual)){
+                    nome_contado = true; //recebe True pois foi encontrado;
+                    break; //sai do lopp
                 }
-
             }
 
+            //se for falso -> siginifica que nome ainda não foi encontrado
+            if(!nome_contado){
+                //Percorre nomes
+                for(String nome: nomes){
+                    if(nome.equals(nome_atual)){ //se nome for igual o nome atual
+                        contador_nome+=1; //atribua mais um em contador_nome
+                    }
+                }
 
+                quantidade.add(contador_nome); //adiciona a quantidade do nome_atual à quantidade
+                nomes_unicos.add(nome_atual); // add o nome atual a nomes_unicos
 
+            }
+        }
 
-        }while (!escolha.equals("-1"));
-
-        System.out.println(nomes);
+        System.out.println("Nomes digitados: "+ nomes_unicos);
+        System.out.println("Quantidade digitadas:" + quantidade);
     }
 }
