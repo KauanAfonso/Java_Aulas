44f51cc9ae05c3de9c9a766825a2eefd3f433eff
kauanAfonso <kauanafon3@gmail.com>
2025-05-14 15:41:51 -0300

    Desafio array nivel dificil parte 2 (2/4) feito, enviando o desafio com tempo

--- src/Arrays/desafio02.java
+++ src/Arrays/desafio02.java
@@ -3,36 +3,45 @@ package Arrays;
 import java.text.Normalizer;
 import java.util.ArrayList;
 import java.util.Scanner;
+import java.util.concurrent.*;
 
 public class desafio02 {
 
     public static void main(String[] args) {
         ArrayList<String> nomes = new ArrayList<String>(); //lista para todos os nomes
         ArrayList<Integer> quantidade = new ArrayList<Integer>();
+        ArrayList<String> nomes_unicos = new ArrayList<String>(); //lista de nomes unicos
 
         Scanner scanner = new Scanner(System.in);
-        ArrayList<String> nomes_unicos = new ArrayList<String>(); //lista de nomes unicos
+
         String escolha;
         Integer contador_nome;
         String palavra_normalizada = "teste";
-        long time = System.currentTimeMillis();
-        long future_time = (int) (time + 10);
+
+        ExecutorService executor = Executors.newSingleThreadExecutor(); //Rodar uma thread separada
+
 
         //Escolha do usuario
         do {
-            for (int i = Math.toIntExact(future_time); i < time ; i-=1) {
-                System.out.println("Digite um nome para a lista ou -1 para sair");
+                try {
+                    System.out.println("Digite um nome para a lista ou -1 para sair, (voce tem 5 segundos)\n");
+                    Mostrar_tempo();
+                    Future<String> future = executor.submit(() -> scanner.next());
+                    escolha = future.get(1, TimeUnit.SECONDS).toLowerCase();
+                    palavra_normalizada = Normalizer.normalize(escolha, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
 
-                escolha = scanner.next().toLowerCase();
-                palavra_normalizada = Normalizer.normalize(escolha, Normalizer.Form.NFD).replaceAll("[^\\p{ASCII}]", "");
+                    if (palavra_normalizada.equals("-1")) {
+                        break;
+                    }
 
-
-                if(palavra_normalizada.equals("-1")){
+                    nomes.add(palavra_normalizada); //adicionando nome que o usuario digitou em nome
+                } catch (TimeoutException e) {
+                    System.out.println("\n\nTEMPO ESGOTADOOOOOOOO!!!!!!!\n");
+                    System.out.println("Programa finalizado...");
                     break;
+                }catch (Exception e){
+                    System.out.println("Algum erro: "+ e.getMessage());
                 }
-                nomes.add(palavra_normalizada); //adicionando nome que o usuario digitou em nomes
-                System.out.println(i);
-            }
 
         }while(!palavra_normalizada.equals("-1")); //Enquanto o usuario não manda '-1'
 
@@ -67,9 +76,22 @@ public class desafio02 {
 
             }
         }
-
         System.out.println("Nomes digitados: "+ nomes_unicos);
         System.out.println("Quantidade digitadas:" + quantidade);
     }
 
+    //metodo para mostrar o tempo na tela
+    public static void Mostrar_tempo(){
+        for (int i = 5; i >0; i--) {
+            System.out.printf(i + "-");
+            try{
+                Thread.sleep(1000);
+            }catch (InterruptedException e){
+                System.out.println("ERRO: " + e);
+            }
+
+        }
+    }
+
 }
+
