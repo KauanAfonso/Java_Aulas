cb19b98c3c489dc5ff9becbd03a810c5b73020fa
kauanAfonso <kauanafon3@gmail.com>
2025-07-23 15:57:41 -0300

    teste

---
+++ src/Sistemas_Compras_POO/Produto.java
@@ -0,0 +1,10 @@
+package Sistemas_Compras_POO;
+
+import java.util.ArrayList;
+
+public class Produto {
+    private String nome;
+    private Float preco;
+    private String[] lista_produtos = {};
+
+}
